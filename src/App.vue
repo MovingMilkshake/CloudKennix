a8c4ef9f5071e39ade353cb4e1376cc1c0d9f725
MovingMilkshake <38225258+MovingMilkshake@users.noreply.github.com>
2019-05-22 18:51:21 +0300

    Navigation is moved to a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,18 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <div>
+      <layout></layout>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
+import Layout from '@/components/Layout'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
-  }
-}
+  components: { Layout },
+  data: () => ({
+    page: "Layout"
+  })
+  };
 </script>
